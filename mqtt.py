c0f7507392edf8ce76bbcb89890a2a6c3f5416a9
Geoff Williams <geoff@declarativesystems.com>
2025-12-23 12:38:54 +1100

    uniquie command id fix

--- mqtt.py
+++ mqtt.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass, asdict
 from paho.mqtt import client as mqtt_client
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes 
-# from paho.mqtt.client import MQTT_CLEAN_START_FIRST_ONLY
 import logging
 import ssl
 from settings import get_config, get_state
@@ -133,7 +132,7 @@ def homeassistant_register(client):
         payload_str = json.dumps(asdict(payload))
         registration = {
             "name": f"Move {direction}",
-            "unique_id": f"pitilt_move_{direction}",
+            "unique_id": f"pitilt_{config.mqtt.client_id}_move_{direction}",
             "payload_press": payload_str,
             "command_topic": get_device_command_topic(),
             "qos": 2,
@@ -161,7 +160,7 @@ def homeassistant_register(client):
         payload_str = json.dumps(asdict(payload))
         registration = {
             "name": f"Location: {location_name}",
-            "unique_id": f"pitilt_location_{location_name}",
+            "unique_id": f"pitilt_{config.mqtt.client_id}_location_{location_name}",
             "payload_press": payload_str,
             "command_topic": get_device_command_topic(),
             "qos": 2,
@@ -232,7 +231,7 @@ def start_mqtt():
 
     elif config.mqtt.tls_enabled:
         if config.mqtt.cacert_path and not Path(config.mqtt.cacert_path).exists():
-            sys.exit(f"MQTT MTLS cacart_path must exist if specified: {config.mqtt.cacert_path}")
+            shutdown_thread(f"MQTT MTLS cacart_path must exist if specified: {config.mqtt.cacert_path}")
         logger.info(f"TLS config: cacert_path={config.mqtt.cacert_path}, ")
         client.tls_set(
             ca_certs=config.mqtt.cacert_path,
@@ -248,7 +247,7 @@ def start_mqtt():
             config.mqtt.password
         )
     else:
-        sys.exit("MQTT authentication is mandatory")
+        shutdown_thread("MQTT authentication is mandatory")
     
     
     #
@@ -311,80 +310,7 @@ def start_mqtt():
         # e.g., re_register_ha()
         time.sleep(0.1)  # optional: prevent busy looping
 
-# def connect_mqtt():
-#     def on_connect(client, userdata, flags, rc):
-#     # For paho-mqtt 2.0.0, you need to add the properties parameter.
-#     # def on_connect(client, userdata, flags, rc, properties):
-#         if rc == 0:
-#             print("Connected to MQTT Broker!")
-#         else:
-#             print("Failed to connect, return code %d\n", rc)
-#     # Set Connecting Client ID
-#     client = mqtt_client.Client(client_id)
 
-#     # For paho-mqtt 2.0.0, you need to set callback_api_version.
-#     # client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
-
-#     # client.username_pw_set(username, password)
-#     client.on_connect = on_connect
-#     client.connect(broker, port)
-#     client.on_disconnect = on_disconnect
-#     return client
-
-
-# FIRST_RECONNECT_DELAY = 1
-# RECONNECT_RATE = 2
-# MAX_RECONNECT_COUNT = 12
-# MAX_RECONNECT_DELAY = 60
-
-# def on_disconnect(client, userdata, rc):
-#     logging.info("Disconnected with result code: %s", rc)
-#     reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
-#     while reconnect_count < MAX_RECONNECT_COUNT:
-#         logging.info("Reconnecting in %d seconds...", reconnect_delay)
-#         time.sleep(reconnect_delay)
-
-#         try:
-#             client.reconnect()
-#             logging.info("Reconnected successfully!")
-#             return
-#         except Exception as err:
-#             logging.error("%s. Reconnect failed. Retrying...", err)
-
-#         reconnect_delay *= RECONNECT_RATE
-#         reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
-#         reconnect_count += 1
-#     logging.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
-
-
-# def subscribe(client: mqtt_client):
-#     def on_message(client, userdata, msg):
-#         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
-#     client.subscribe(topic)
-#     client.on_message = on_message
-
-# def publish(client):
-#     msg_count = 1
-#     while True:
-#         time.sleep(1)
-#         msg = f"messages: {msg_count}"
-#         result = client.publish(topic, msg)
-#         # result: [0, 1]
-#         status = result[0]
-#         if status == 0:
-#             print(f"Send `{msg}` to topic `{topic}`")
-#         else:
-#             print(f"Failed to send message to topic {topic}")
-#         msg_count += 1
-#         if msg_count > 5:
-#             break
-
-
-# def run():
-#     client = connect_mqtt()
-#     subscribe(client)
-#     client.loop_forever()
 
 
 def shutdown_thread(msg):
